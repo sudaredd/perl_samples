b1e5d8b63a076e98ac22bd8c03b96b23b709dab0
sudaredd <sudaredd@gmail.com>
2018-06-03 22:12:48 -0400

    initial commits

---
+++ variables_d.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+$age=25;
+$name="darsan";
+$sal=10120.34;
+
+print "Age =$age\n";
+print "Name =$name\n";
+print "Salary =$sal\n";
+
+
+#Arrays
+@ages = (10,20,25);
+@names=("darsan","reddy","kasaireddy");
+
+print "\$ages[0]:$ages[0]\n";
+print "\$ages[1]:$ages[1]\n";
+print "\$ages[2]:$ages[2]\n";
+print "\$names[0]:$names[0]\n";
+print "\$names[1]:$names[1]\n";
+print "\$names[2]:$names[2]\n";
+
+#Hash variables
+%data = ('John Paul', 45, 'Lisa', 30, 'Kumar', 40);
+print "\$data{'John Paul'} = $data{'John Paul'}\n";
+print "\$data{'Lisa'} = $data{'Lisa'}\n";
+print "\$data{'Kumar'} = $data{'Kumar'}\n";
